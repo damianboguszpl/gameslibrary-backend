f30161806c67fa8ef680c893ad579918a91387b1
Paweł Ciszewski <kardahim@gmail.com>
2022-10-11 17:44:15 +0200

    Initial commit

---
+++ src/main/java/pl/pollub/gameslibrary/GamesLibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.pollub.gameslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesLibraryApplication.class, args);
+	}
+
+}
