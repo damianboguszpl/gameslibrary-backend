611905b6430d49f000e4a2b84f90f0e805778331
Paweł Ciszewski <kardahim@gmail.com>
2022-10-17 16:35:22 +0200

    remove jdbc, add jpa, add example model, controller and repository

---
+++ src/main/java/pl/pollub/gameslibrary/Controllers/UserController.java
@@ -0,0 +1,37 @@
+package pl.pollub.gameslibrary.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.pollub.gameslibrary.Models.User;
+import pl.pollub.gameslibrary.Repositories.UserRepository;
+
+@Controller
+@RequestMapping(path="/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody String addNewUser (@RequestParam String name
+            , @RequestParam String email) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/pl/pollub/gameslibrary/Repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package pl.pollub.gameslibrary.Repositories;
+
+
+import org.springframework.data.repository.CrudRepository;
+import pl.pollub.gameslibrary.Models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
