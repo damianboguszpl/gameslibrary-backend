99c46af026a1a4b2476304f45c4d1e7f3126b6c9
Paweł Ciszewski <kardahim@gmail.com>
2022-10-19 12:45:37 +0200

    rebuild User model

--- src/main/java/pl/pollub/gameslibrary/Models/User.java
+++ src/main/java/pl/pollub/gameslibrary/Models/User.java
@@ -1,41 +1,25 @@
 package pl.pollub.gameslibrary.Models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+import lombok.ToString;
 
-@Entity // This tells Hibernate to make a table out of this class
+import javax.persistence.*;
+
+@Entity
+@Data
+@ToString
 public class User {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
 
-    private String name;
+    @Column(nullable = false)
+    private String login;
 
+    @Column(nullable = false)
     private String email;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    @Column(nullable = false)
+    private String password;
 }
