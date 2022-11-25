07c755035a44eca3f88876e8d6a15971b25732a2
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-11-25 07:52:31 +0100

    redefine add, edit & delete methods in UserService

--- src/main/java/pl/pollub/gameslibrary/Services/UserService.java
+++ src/main/java/pl/pollub/gameslibrary/Services/UserService.java
@@ -1,12 +1,15 @@
 package pl.pollub.gameslibrary.Services;
 
+import org.springframework.http.ResponseEntity;
+import pl.pollub.gameslibrary.Models.Utility.DetailedResponse;
+import pl.pollub.gameslibrary.Exceptions.Exceptions.*;
 import pl.pollub.gameslibrary.Models.User;
 
 public interface UserService {
     Iterable<User> getAll();
     User getById(Long id);
     User getByEmail(String email);
-    User edit(User newUser, Long id);
-    User add(User user);
-    User delete(Long id);
+    User edit(User newUser, Long id) throws UserNotFoundException, IncorrectRequestDataException;
+    ResponseEntity<DetailedResponse> add(User user);
+    ResponseEntity<DetailedResponse> delete(Long id) throws UserNotFoundException;
 }
