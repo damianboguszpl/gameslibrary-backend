c3503197f1c33614c5076265a820f8419a3d85fc
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-11-26 19:47:36 +0100

    add findByUserIs & findByUserAndApp method to FavouriteAppRepository

--- src/main/java/pl/pollub/gameslibrary/Repositories/FavouriteAppRepository.java
+++ src/main/java/pl/pollub/gameslibrary/Repositories/FavouriteAppRepository.java
@@ -3,9 +3,14 @@ package pl.pollub.gameslibrary.Repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import pl.pollub.gameslibrary.Models.App;
 import pl.pollub.gameslibrary.Models.FavouriteApp;
+import pl.pollub.gameslibrary.Models.User;
+
+import java.util.List;
 
 @Repository
 public interface FavouriteAppRepository extends CrudRepository<FavouriteApp, Long> {
-
+    List<FavouriteApp> findByUserIs(User user);
+    FavouriteApp findByUserAndApp(User user, App app);
 }
