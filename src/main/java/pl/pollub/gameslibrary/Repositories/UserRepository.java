4bc3ecb3ca0815e5a86c9281a61e8b9740942a38
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-11-26 19:45:24 +0100

    add findById method to UserRepository

--- src/main/java/pl/pollub/gameslibrary/Repositories/UserRepository.java
+++ src/main/java/pl/pollub/gameslibrary/Repositories/UserRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.pollub.gameslibrary.Models.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(String email);
+    Optional<User> findById(Long id);
     User findByLogin(String login);
 }
