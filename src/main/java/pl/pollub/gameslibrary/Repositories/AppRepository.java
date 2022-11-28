014951697c280f438c32af3f18e3bfb869475ce4
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-11-28 12:43:50 +0100

    add findByType method to AppRepository

--- src/main/java/pl/pollub/gameslibrary/Repositories/AppRepository.java
+++ src/main/java/pl/pollub/gameslibrary/Repositories/AppRepository.java
@@ -5,4 +5,6 @@ import org.springframework.stereotype.Repository;
 import pl.pollub.gameslibrary.Models.App;
 
 @Repository
-public interface AppRepository extends CrudRepository<App, Long> {}
+public interface AppRepository extends CrudRepository<App, Long> {
+    Iterable<App> findByType(String type);
+}
