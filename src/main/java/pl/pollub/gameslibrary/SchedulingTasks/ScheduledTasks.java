41a92b9a304b537d55a3bfa7d37f004a44e2579c
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-12-02 16:30:34 +0100

    clean unnecessary comments

--- src/main/java/pl/pollub/gameslibrary/SchedulingTasks/ScheduledTasks.java
+++ src/main/java/pl/pollub/gameslibrary/SchedulingTasks/ScheduledTasks.java
@@ -15,12 +15,6 @@ public class ScheduledTasks {
     AppService appService;
     // cron second(0-59) minute(0-59) hour(0-59) day_of_the_month(1-31) month(1-12) day_of_the_week(0-6)
 
-//    // execute at 08.00 AM every day
-//    @Scheduled(cron = "0 0 8 * * *")
-//    public void test() {
-//        System.out.println("test");
-//    }
-
     // execute at 16.00 PM every day
     @Scheduled(cron = "0 13 16 * * *")
     public void pullNewAppFromSteamApi() throws JSONException, IOException, InterruptedException {
