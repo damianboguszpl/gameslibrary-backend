908ec9cac7f7c941f6bd6bcd01c22e1c5025d053
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-12-02 16:24:14 +0100

    add & redefine pullFromApi method in SteamApiService, so it works in two modes

--- src/main/java/pl/pollub/gameslibrary/Services/SteamApiService.java
+++ src/main/java/pl/pollub/gameslibrary/Services/SteamApiService.java
@@ -21,173 +21,107 @@ public interface SteamApiService {
     String appListURL = "http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json";
     String appDetailsURL = "https://store.steampowered.com/api/appdetails?appids=";
 
-    static void rebase(AppService appService) throws IOException, InterruptedException, JSONException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest appListRequests = HttpRequest.newBuilder().uri(URI.create(appListURL)).build();
+    static void pullFromApi(AppService appService, String mode) throws IOException, InterruptedException, JSONException {
+        System.out.println("SteamApiService: Pull procedure started. Mode: '" + mode + "'.");
+        if(Objects.equals(mode, "PULL_NEW_ONLY") || Objects.equals(mode, "FULL_UPDATE")) {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest appListRequests = HttpRequest.newBuilder().uri(URI.create(appListURL)).build();
+            HttpResponse<String> appListResponse = null;
 
-        HttpResponse<String> appListResponse = client.send(appListRequests, HttpResponse.BodyHandlers.ofString());
-        JSONObject appListObject = new JSONObject(appListResponse.body());
-        appListObject = appListObject.getJSONObject("applist");
-        JSONArray appListArray = appListObject.getJSONArray("apps");
-
-        for (int i = 0; i < appListArray.length(); i++) {
-            Object appListArrayElement = appListArray.get(i);
-            JSONObject myJSONObject = (JSONObject) appListArrayElement;
-            Long id = myJSONObject.getLong("appid");
-            String name = myJSONObject.getString("name");
-
-            System.out.println(i +"> appId: " + id + " & name: " + name);
-
-            if (name.length()!=0) {
-                HttpRequest appDetailsRequest = HttpRequest.newBuilder().uri(URI.create(appDetailsURL + id)).build();
-                HttpResponse<String> appDetailsResponse = appListResponse;	// not right
-
-                boolean goOn = false;
-                while(!goOn) {
-                    appDetailsResponse = client.send(appDetailsRequest, HttpResponse.BodyHandlers.ofString());
-                    if(appDetailsResponse.statusCode() != 200) {
-                        System.out.println(appDetailsResponse.statusCode() + "... too many requests... waiting 3s");
-                        TimeUnit.MILLISECONDS.sleep(3000);
+            boolean shouldWaitForAccess = true;
+            boolean infoShown = false;
+            while(shouldWaitForAccess) {
+                appListResponse = client.send(appListRequests, HttpResponse.BodyHandlers.ofString());
+                if(appListResponse.statusCode() != 200) {
+                    if(!infoShown) {
+                        System.out.println("SteamApiService: Public Steam API Request responseCode: " + appListResponse.statusCode() + " -> too many requests. Only 200 requests per 5 mins allowed. Waiting for access...");
+                        infoShown = true;
                     }
-                    else { goOn = true; }
+                    TimeUnit.MILLISECONDS.sleep(3000);
                 }
-
-                JSONObject appDetailsObject = new JSONObject(appDetailsResponse.body());
-                JSONObject appDetails = appDetailsObject.getJSONObject(myJSONObject.getString("appid"));
-                try {
-                    boolean success = appDetails.getBoolean(("success"));
-                    if (success)
-                    {
-                        appDetails = appDetails.getJSONObject("data");
-
-                        String title = appDetails.getString("name");
-                        String type = appDetails.getString("type");
-                        String description = appDetails.getString("detailed_description");
-                        String shortDescription = appDetails.getString("short_description");
-                        String screenshotLink = appDetails.getString("header_image");
-                        String developers = appDetails.getString("developers");
-                        String publishers = appDetails.getString("publishers");
-
-                        App app = new App();
-                        app.setId(id);
-                        app.setTitle(title);
-                        app.setType(type);
-                        app.setDescription(description);
-                        app.setShortDescription(shortDescription);
-                        app.setScreenshotLink(screenshotLink);
-                        app.setDevelopers(developers);
-                        app.setPublishers(publishers);
-//                        System.out.println(app);
-
-                        App existingApp = appService.getById(id);
-                        if (existingApp == null) {
-                            System.out.println("app to be added");
-                            appService.add(app);
-                        }
-                        else {
-                            System.out.println("app to be updated");
-                            appService.edit(app, id);
-                        }
-                    }
-                }
-                catch (JSONException exception) {
-                    System.out.println(exception.getMessage());
-                }
+                else { shouldWaitForAccess = false; }
             }
-        }
-    }
 
-    static void pullNewApps(AppService appService) throws IOException, InterruptedException, JSONException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest appListRequests = HttpRequest.newBuilder().uri(URI.create(appListURL)).build();
-        
-//        HttpResponse<String> appListResponse = client.send(appListRequests, HttpResponse.BodyHandlers.ofString());
-        HttpResponse<String> appListResponse = null;
-        
-        boolean goOn1 = false;
-        while(!goOn1) {
-            appListResponse = client.send(appListRequests, HttpResponse.BodyHandlers.ofString());
-            if(appListResponse.statusCode() != 200) {
-                System.out.println(appListResponse.statusCode() + "... too many requests... waiting 3s");
-                TimeUnit.MILLISECONDS.sleep(3000);
-            }
-            else { goOn1 = true; }
-        }
-//        
+            JSONObject appListObject = new JSONObject(appListResponse.body());
+            appListObject = appListObject.getJSONObject("applist");
+            JSONArray appListArray = appListObject.getJSONArray("apps");
+            for (int i = 0; i < appListArray.length(); i++) {
+                Object appListArrayElement = appListArray.get(i);
+                JSONObject myJSONObject = (JSONObject) appListArrayElement;
+                Long currentAppId = myJSONObject.getLong("appid");
+                String currentAppName = myJSONObject.getString("name");
 
-        JSONObject appListObject = new JSONObject(appListResponse.body());
-        appListObject = appListObject.getJSONObject("applist");
-        JSONArray appListArray = appListObject.getJSONArray("apps");
+                if (currentAppName.length()!=0) {
+                    App existingApp = appService.getById(currentAppId);
+                    if(existingApp != null && Objects.equals(mode, "PULL_NEW_ONLY")) {
+                        System.out.println("SteamApiService: Skipping existing app with id:" + currentAppId);
+                    } else {
+                        HttpRequest appDetailsRequest = HttpRequest.newBuilder().uri(URI.create(appDetailsURL + currentAppId)).build();
+                        HttpResponse<String> appDetailsResponse = appListResponse;	// not right
 
-        for (int i = 0; i < appListArray.length(); i++) {
-            Object appListArrayElement = appListArray.get(i);
-            JSONObject myJSONObject = (JSONObject) appListArrayElement;
-            Long id = myJSONObject.getLong("appid");
-            String name = myJSONObject.getString("name");
-
-            System.out.println(i +"> appId: " + id + " & name: " + name);
-
-            if (name.length()!=0) {
-                App existingApp = appService.getById(id);
-                if (existingApp == null) {
-                    HttpRequest appDetailsRequest = HttpRequest.newBuilder().uri(URI.create(appDetailsURL + id)).build();
-                    HttpResponse<String> appDetailsResponse = appListResponse;	// not right
-
-                    boolean goOn = false;
-                    while(!goOn) {
-                        appDetailsResponse = client.send(appDetailsRequest, HttpResponse.BodyHandlers.ofString());
-                        if(appDetailsResponse.statusCode() != 200) {
-                            System.out.println(appDetailsResponse.statusCode() + "... too many requests... waiting 3s");
-                            TimeUnit.MILLISECONDS.sleep(3000);
-                        }
-                        else { goOn = true; }
-                    }
-                    System.out.println(appDetailsResponse.statusCode());
-                    System.out.println(appDetailsResponse.body());
-                    JSONObject appDetailsObject = new JSONObject(appDetailsResponse.body());
-                    JSONObject appDetails = appDetailsObject.getJSONObject(myJSONObject.getString("appid"));
-                    try {
-                        boolean success = appDetails.getBoolean(("success"));
-                        if (success)
-                        {
-                            appDetails = appDetails.getJSONObject("data");
-
-                            String title = appDetails.getString("name");
-                            String type = appDetails.getString("type");
-                            String description = appDetails.getString("detailed_description");
-                            String shortDescription = appDetails.getString("short_description");
-                            String screenshotLink = appDetails.getString("header_image");
-                            String developers = appDetails.getString("developers");
-                            String publishers = appDetails.getString("publishers");
-
-                            App app = new App();
-                            app.setId(id);
-                            app.setTitle(title);
-                            app.setType(type);
-                            app.setDescription(description);
-                            app.setShortDescription(shortDescription);
-                            app.setScreenshotLink(screenshotLink);
-                            app.setDevelopers(developers);
-                            app.setPublishers(publishers);
-//                            System.out.println(app);
-
-                            if(Objects.equals(type, "game") || Objects.equals(type, "dlc")) { // move it up
-                                System.out.println("app to be added");
-                                appService.add(app);
+                        shouldWaitForAccess = true;
+                        infoShown = false;
+                        while(shouldWaitForAccess) {
+                            appDetailsResponse = client.send(appDetailsRequest, HttpResponse.BodyHandlers.ofString());
+                            if(appDetailsResponse.statusCode() != 200) {
+                                if(!infoShown) {
+                                    System.out.println("SteamApiService: Public Steam API Request responseCode: " + appDetailsResponse.statusCode() + " -> too many requests. Only 200 requests per 5 mins allowed. Waiting for access...");
+                                    infoShown = true;
+                                }
+                                TimeUnit.MILLISECONDS.sleep(3000);
                             }
-                            else{
-                                System.out.println("app is not a game nor dlc");
+                            else { shouldWaitForAccess = false; }
+                        }
+
+                        try {
+                            JSONObject appDetailsObject = new JSONObject(appDetailsResponse.body());
+                            JSONObject appDetails = appDetailsObject.getJSONObject(myJSONObject.getString("appid"));
+                            boolean success = appDetails.getBoolean(("success"));
+                            if (success)
+                            {
+                                appDetails = appDetails.getJSONObject("data");
+
+                                String title = appDetails.getString("name");
+                                String type = appDetails.getString("type");
+                                String description = appDetails.getString("detailed_description");
+                                String shortDescription = appDetails.getString("short_description");
+                                String screenshotLink = appDetails.getString("header_image");
+                                String developers = appDetails.getString("developers");
+                                String publishers = appDetails.getString("publishers");
+
+                                App app = new App();
+                                app.setId(currentAppId);
+                                app.setTitle(title);
+                                app.setType(type);
+                                app.setDescription(description);
+                                app.setShortDescription(shortDescription);
+                                app.setScreenshotLink(screenshotLink);
+                                app.setDevelopers(developers.isEmpty() ? developers : " ");
+                                app.setPublishers(publishers.isEmpty() ? publishers : " ");
+
+                                if (existingApp == null) {
+                                    if(Objects.equals(type, "game") || Objects.equals(type, "dlc") || Objects.equals(type, "demo")) { // move it up
+                                        System.out.println("SteamApiService: Adding new app: appId: " + currentAppId + " | name: " + currentAppName);
+                                        appService.add(app);
+                                    }
+                                    else{
+                                        System.out.println("SteamApiService: App is neither ot type 'game', 'dlc', nor 'demo' -> skipping.");
+                                    }
+                                }
+                                else if(Objects.equals(mode, "FULL_UPDATE")){
+                                    System.out.println("SteamApiService: Updating an existing app: appId: " + currentAppId + " | name: " + currentAppName);
+                                    appService.edit(app, currentAppId);
+                                }
                             }
                         }
+                        catch (JSONException exception) {
+                            System.out.println("SteamApiService Exception: " + exception.getMessage());
+                        }
                     }
-                    catch (JSONException exception) {
-                         System.out.println(exception.getMessage());
-                    }
-                }
-                else {
-                    System.out.println("app to be skipped");
                 }
             }
-        }
+        } else
+            System.out.println("SteamApiService: Incorrect mode '" + mode + "' for pullFromApi() method.");
+        System.out.println("SteamApiService: Pull procedure ended.");
     }
 }
